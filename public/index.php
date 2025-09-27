6d239dcd34452e6b44659e39689654111aef9132
Manuel Kießling <manuel@kiessling.net>
2025-09-27 13:39:42 +0200

    Increased PHPStan level from 9 to 10

--- public/index.php
+++ public/index.php
@@ -9,6 +9,12 @@ require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 date_default_timezone_set(Timezone::UTC->value);
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return function (
+    array $context
+) {
+    if (!is_string($context['APP_ENV'])) {
+        throw new ValueError('APP_ENV should be a string');
+    }
+
+    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };

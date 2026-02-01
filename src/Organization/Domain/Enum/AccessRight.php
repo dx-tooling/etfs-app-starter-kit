32f30a14eb3fc6ac97171eb68820e91861bff9a6
Manuel Kießling <manuel@kiessling.net>
2026-02-01 10:47:09 +0100

    docs, cleanups

--- src/Organization/Domain/Enum/AccessRight.php
+++ src/Organization/Domain/Enum/AccessRight.php
@@ -11,6 +11,4 @@ enum AccessRight: string
     case INVITE_ORGANIZATION_MEMBERS           = 'invite_organization_members';
     case SEE_ORGANIZATION_GROUPS_AND_MEMBERS   = 'see_organization_groups_and_members';
     case MOVE_ORGANIZATION_MEMBERS_INTO_GROUPS = 'move_organization_members_into_groups';
-    case EDIT_CUSTOM_LOGO_SETTINGS             = 'edit_custom_logo_settings';
-    case EDIT_CUSTOM_DOMAIN_SETTINGS           = 'edit_custom_domain_settings';
 }
